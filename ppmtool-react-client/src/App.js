c1d7a06c14169b7adf51fd24516f2ac92c649ce5
Turan <tshukurbeyli@gmail.com>
2020-02-26 14:36:31 +0900

    navbar and items

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -2,9 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import Layout from "./components/Layout/Layout";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
   return (
     <div className="App">
+      <Layout />
       <Dashboard />
     </div>
   );
