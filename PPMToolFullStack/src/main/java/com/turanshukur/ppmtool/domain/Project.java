3dffa781fd25ad5f3ca8f32fccf9e7f37684b818
Turan <tshukurbeyli@gmail.com>
2020-03-04 15:33:24 +0900

    Project Entity

--- PPMToolFullStack/src/main/java/com/turanshukur/ppmtool/domain/Project.java
+++ PPMToolFullStack/src/main/java/com/turanshukur/ppmtool/domain/Project.java
@@ -7,7 +7,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Date;
-
+// Project Entity
 @Entity
 @Table(name = "project")
 public class Project {
